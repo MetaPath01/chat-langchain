e3e29ac3b50f46aeeafaad890010cd39c1bae680
holynull <holynull@126.com>
2023-05-04 10:35:30 +0800

    111

--- ingest_swft.py
+++ ingest_swft.py
@@ -9,6 +9,8 @@ from langchain.vectorstores.faiss import FAISS
 from dotenv import load_dotenv
 from pathlib import Path
 import sys
+import os
+import argparse
 
 if getattr(sys, 'frozen', False):
     script_location = Path(sys.executable).parent.resolve()
@@ -16,9 +18,16 @@ else:
     script_location = Path(__file__).parent.resolve()
 load_dotenv(dotenv_path=script_location / '.env')
 
+parser = argparse.ArgumentParser(description='Ingest data.')
+parser.add_argument('-f', '--fileName',
+                    help="Twitter's user name")
+args = parser.parse_args()
+fileNmae = args.fileName
+
+
 def ingest_docs():
     """Get documents from web pages."""
-    loader = CSVLoader("dataset/dataset 20230426-121810.csv")
+    loader = CSVLoader(fileNmae)
     raw_documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
@@ -26,7 +35,13 @@ def ingest_docs():
     )
     documents = text_splitter.split_documents(raw_documents)
     embeddings = OpenAIEmbeddings(model="gpt-4")
-    vectorstore = FAISS.from_documents(documents, embeddings)
+    if os.path.exists(f"{script_location}/vectorstore.pkl"):
+        with open("vectorstore.pkl", "rb") as f:
+            vectorstore = pickle.load(f)
+        n_vectorstore = FAISS.from_documents(documents, embeddings)
+        vectorstore.merge_from(n_vectorstore)
+    else:
+        vectorstore = FAISS.from_documents(documents, embeddings)
 
     # Save vectorstore
     with open("vectorstore.pkl", "wb") as f:
